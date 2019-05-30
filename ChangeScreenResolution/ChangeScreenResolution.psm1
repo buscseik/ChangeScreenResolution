1e62e1e11cbd671672e1f5c30946d64b3e6939b1
Krisztian.Buscsei <buscseik@gmail.com>
2019-05-30 19:57:43 +0100

    Initial version

---
+++ ChangeScreenResolution/ChangeScreenResolution.psm1
@@ -0,0 +1,10 @@
+﻿#region import script
+. $PSScriptRoot\ChangeScreenResolution.ps1
+
+
+#endregion import script
+
+#region export module member
+export-modulemember -function Set-ScreenResolution
+
+#endregion export module member
